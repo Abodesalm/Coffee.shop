a25b5afae4af70420e4abbd9af3a23a1b2dc3d97
Abodesalm <114345535+Abodesalm@users.noreply.github.com>
2023-05-26 16:05:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+
+let vis1_1 = document.getElementById("vis1_1");
+let vis1_2 = document.getElementById("vis1_2");
+let vis1_3 = document.getElementById("vis1_3");
+let vis1_4 = document.getElementById("vis1_4");
+let vis1_5 = document.getElementById("vis1_5");
+let vis1_6 = document.getElementById("vis1_6");
+let vis1_7 = document.getElementById("vis1_7");
+let vis1_8 = document.getElementById("vis1_8");
+let vis1_9 = document.getElementById("vis1_9");
+let vis1_10 = document.getElementById("vis1_10");
+let vis1_11 = document.getElementById("vis1_11");
+let vis1_12 = document.getElementById("vis1_12");
+let vis1_13 = document.getElementById("vis1_13");
+let vis1_14 = document.getElementById("vis1_14");
+
+let vis2_1 = document.getElementById("vis2_1");
+let vis2_2 = document.getElementById("vis2_2");
+let vis2_3 = document.getElementById("vis2_3");
+let vis2_4 = document.getElementById("vis2_4");
+let vis2_5 = document.getElementById("vis2_5");
+
+
+
+let vis3_1 = document.getElementById("vis3_1");
+
+let products = document.getElementsByClassName("pro");
+
+
+let sm = document.getElementById("sm");
+let sm2 = document.getElementById("sm2");
+let sm3 = document.getElementById("sm3");
+
+
+
+sm.onclick = function(){
+  vis1_1.classList.toggle("vis1")
+  vis1_2.classList.toggle("vis1")
+  vis1_3.classList.toggle("vis1")
+  vis1_4.classList.toggle("vis1")
+  vis1_5.classList.toggle("vis1")
+  vis1_6.classList.toggle("vis1")
+  vis1_7.classList.toggle("vis1")
+  vis1_8.classList.toggle("vis1")
+  vis1_9.classList.toggle("vis1")
+  vis1_10.classList.toggle("vis1")
+  vis1_11.classList.toggle("vis1")
+  vis1_12.classList.toggle("vis1")
+  vis1_13.classList.toggle("vis1")
+  vis1_14.classList.toggle("vis1")
+}
+
+sm2.onclick= function(){
+  vis2_1.classList.toggle("vis2");
+  vis2_2.classList.toggle("vis2");
+  vis2_3.classList.toggle("vis2");
+  vis2_4.classList.toggle("vis2");
+  vis2_5.classList.toggle("vis2");
+}
+sm3.onclick= function(){
+  vis3_1.classList.toggle("vis3")
+}
